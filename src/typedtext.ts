def2fa106668788194728e2798fd3356627f9f29
Philippe <philippebraum@pm.me>
2022-12-21 13:09:44 -0500

    version 0.1.32

--- src/typedtext.ts
+++ src/typedtext.ts
@@ -8,7 +8,7 @@ Copyright (C) Philippe Braum (www.pbr.plus)
 Available under the MIT license
 */
 
-const typedtextjsVersionId = "0.1.31:122022";
+const typedtextjsVersionId = "0.1.32:122022";
 
 const defaultOptions = {
     /**
